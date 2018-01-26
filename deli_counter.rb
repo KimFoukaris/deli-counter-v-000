95dfb97927f5f64ceb9d779e0996e9ef675f96ea
Kim Foukaris <kimfoukaris@outlook.com>
2018-01-26 23:45:15 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,7 @@
-# Write your code here.
+def line(katz_deli)
+  if katz_deli.length ==0
+    puts "There is nobody waiting to be served!"
+  else
+    puts "The line is currently: "
+    katz_deli.each_with_index {|name, index| puts "#{index+1}. #{name} "}
+end
